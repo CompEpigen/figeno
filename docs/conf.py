923635b18287e5bfd5afa39ef79560218e1b649b
e-sollier <etienne.sollier@gmail.com>
2024-02-09 17:38:00 +0100

    update readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -28,8 +28,8 @@ html_theme = 'alabaster'
 html_static_path = ['_static']
 
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+html_theme = 'sphinx_rtd_theme'
 
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
