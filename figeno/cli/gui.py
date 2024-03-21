ad2f130f70dc5888e20fb2558c5d4237f6deada3
e-sollier <etienne.sollier@gmail.com>
2024-03-21 13:51:33 +0100

    update cli for new gui

--- figeno/cli/gui.py
+++ figeno/cli/gui.py
@@ -1,7 +1,10 @@
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 def main(args):
-    if not args.webview:
+    if args.server:
+        from figeno.gui import gui_server
+        gui_server.main(args)
+    elif not args.webview:
         from figeno.gui import gui_browser
         gui_browser.main(args)
     else:
@@ -10,9 +13,11 @@ def main(args):
 
 def argparser():
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter,add_help=False)
-    parser.add_argument('-w','--webview', dest="webview",action='store_true',help="If set, start the GUI in webview (using pywebview) instead of in the browser..")
+    parser.add_argument('-w','--webview', dest="webview",action='store_true',help="If set, start the GUI in webview (using pywebview) instead of in the browser.")
+    parser.add_argument('-s','--server', dest="server",action='store_true',help="If set, start the GUI in server mode, with filedialogs in the browser.")
     parser.add_argument("-p","--port",type=int,default=5000, help="Port, only used in browser mode.")
     parser.add_argument('--debug', action='store_true',help="If set, will provide more debugging information.")
     parser.set_defaults(debug=False)
     parser.set_defaults(webview=False)
+    parser.set_defaults(server=False)
     return parser
